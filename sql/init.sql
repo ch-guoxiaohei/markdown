1bcc95ac5b2826aa9c3b31e35e76ab549f89237e
guoxiaohei <842553705@qq.com>
2021-11-28 19:11:03 +0800

    add new category .

--- sql/init.sql
+++ sql/init.sql
@@ -27,5 +27,13 @@ create table markdowneditor.t_category
   dt_update_time timestamp
 );
 
-insert into markdowneditor.t_category(c_id,c_name,dt_create_time,dt_update_time) values('83b2416c24414eb68098a9fae8fabcf2','java',now(),now());
-
+insert into markdowneditor.t_category(c_id,c_name,dt_create_time,dt_update_time) values('83b2416c24414eb68098a9fae8fabcf0','Shell',now(),now());
+insert into markdowneditor.t_category(c_id,c_name,dt_create_time,dt_update_time) values('83b2416c24414eb68098a9fae8fabcf1','Operate System',now(),now());
+insert into markdowneditor.t_category(c_id,c_name,dt_create_time,dt_update_time) values('83b2416c24414eb68098a9fae8fabcf2','Java',now(),now());
+insert into markdowneditor.t_category(c_id,c_name,dt_create_time,dt_update_time) values('83b2416c24414eb68098a9fae8fabcf3','English',now(),now());
+insert into markdowneditor.t_category(c_id,c_name,dt_create_time,dt_update_time) values('83b2416c24414eb68098a9fae8fabcf4','Python',now(),now());
+insert into markdowneditor.t_category(c_id,c_name,dt_create_time,dt_update_time) values('83b2416c24414eb68098a9fae8fabcf5','Micro Service',now(),now());
+insert into markdowneditor.t_category(c_id,c_name,dt_create_time,dt_update_time) values('83b2416c24414eb68098a9fae8fabcf6','Test',now(),now());
+insert into markdowneditor.t_category(c_id,c_name,dt_create_time,dt_update_time) values('83b2416c24414eb68098a9fae8fabcf7','Big Data',now(),now());
+insert into markdowneditor.t_category(c_id,c_name,dt_create_time,dt_update_time) values('83b2416c24414eb68098a9fae8fabcf8','Life',now(),now());
+insert into markdowneditor.t_category(c_id,c_name,dt_create_time,dt_update_time) values('83b2416c24414eb68098a9fae8fabcf9','Frontend',now(),now());
