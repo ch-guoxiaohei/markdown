7a64ab77c45844787905a120448c34465d5b34ea
guoxiaohei <842553705@qq.com>
2021-11-28 13:32:54 +0800

    add security

--- src/main/java/com/guoxiaohei/markdown/MarkdownApplication.java
+++ src/main/java/com/guoxiaohei/markdown/MarkdownApplication.java
@@ -2,10 +2,12 @@ package com.guoxiaohei.markdown;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableWebSecurity
 public class MarkdownApplication {
 
   public static void main(String[] args) {
